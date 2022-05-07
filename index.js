13fa0bdcdd45c2f2baf4e3767d5e4d260fa66e4a
James Tennant <jtennant@ucsc.edu>
2022-05-06 19:17:53 -0700

    setup socketio chat template

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const path = require("path");
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const gamedirectory = path.join(__dirname, "public");
+
+app.use(express.static(gamedirectory));
 
 server.listen(port);
+
+var rooms = [];
+var usernames = [];
+
+io.on('connection', function(socket){
+
+  socket.on("join", function(room, username){
+    if (username != ""){
+      rooms[socket.id] = room;
+      usernames[socket.id] = username;
+      socket.leaveAll();
+      socket.join(room);
+      io.in(room).emit("recieve", "Server : " + username + " has entered the chat.");
+      socket.emit("join", room);
+    }
+  })
+
+  socket.on("send", function(message){
+    io.in(rooms[socket.id]).emit("recieve", usernames[socket.id] +" : " + message);
+  })
+
+  socket.on("recieve", function(message){
+    socket.emit("recieve", message);
+  })
+})
