e5971f2741c07ee5908243db30e4aa48bf3f9ca6
James Tennant <jtennant@ucsc.edu>
2022-05-13 22:33:56 -0700

    render scene from gamestate locally

---
+++ README.md
@@ -0,0 +1,13 @@
+# Slughub
+
+a virtual space where users can move
+
+## Outline
+
+1. connect to server
+
+2. receive continous updates to gamestate
+
+3. continuously render the gamestate
+
+4. send input to server
--- main.js
+++ main.js
@@ -1,29 +1,83 @@
-let gamestate = {}
+let gamestate = {}; // gamestate is dictionary of players
+
 
 // establish connection to server
 
 // upon recieving update from server, update gamestate
 
 
+// allow user to modify gamestate (temporary)
+let add = document.getElementById('add').onclick = () => {
+    let id = document.querySelector('input').value;
+    gamestate[id] = {
+        x: -3 + Math.random() * 6,
+        y: -3 + Math.random() * 6,
+        color: Math.random() * 0xffffff
+    };
+};
+let remove = document.getElementById('remove').onclick = () => {
+    let id = document.querySelector('input').value;
+    delete gamestate[id];
+};
 
 
-// create a scene
+// create scene
+let scene = new THREE.Scene();
 
+// create camera
+let camera = new THREE.PerspectiveCamera();
+camera.position.z = 10;
 
-// create a camera
+// create renderer
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(400, 400);
+document.body.appendChild(renderer.domElement);
+
+// define player geometry
+let geometry = new THREE.ConeGeometry(0.5);
 
 // continuously render the scene from the camera
+let meshes = {}; // need to keep track of meshes in scene
+animate();
 
-// before each render, update the scene based on gamestate
+// animate
+function animate() {
+    requestAnimationFrame(animate);
+
+    update(); // before each render, update the scene based on gamestate
+
+    renderer.render(scene, camera);
+};
+
+
+// update scene based on gamestate
+function update() {
+
+    // add new players
+    for (let id in gamestate) {
+        if (id in meshes) continue;
+
+        let material = new THREE.MeshMatcapMaterial({ color: gamestate[id].color });
+        let mesh = new THREE.Mesh(geometry, material);
+        mesh.position.x = gamestate[id].x;
+        mesh.position.y = gamestate[id].y;
+        scene.add(mesh);
+
+        meshes[id] = {mesh, material, geometry};
+    }
+
+    // remove abscent players
+    for (let id in meshes) {
+        if (id in gamestate) continue;
+        meshes[id].material.dispose();
+        meshes[id].geometry.dispose();
+        scene.remove(meshes[id].mesh);
+        renderer.renderLists.dispose();
+        delete meshes[id];
+    }
+}
 
 // configure input controls
 
-    // upon user input, send input to server
+// upon user input, send input to server
 
-// 1. connect to server
-
-// 2. receive continous updates to gamestate
-
-// 3. continuously render the gamestate
-
-// 4. send input to server
