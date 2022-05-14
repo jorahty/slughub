010cfbfe5c7a2406f039d81becebc5cbbb31440e
James Tennant <jtennant@ucsc.edu>
2022-05-13 22:15:15 -0700

    draft outline

---
+++ main.js
@@ -0,0 +1,29 @@
+let gamestate = {}
+
+// establish connection to server
+
+// upon recieving update from server, update gamestate
+
+
+
+
+// create a scene
+
+
+// create a camera
+
+// continuously render the scene from the camera
+
+// before each render, update the scene based on gamestate
+
+// configure input controls
+
+    // upon user input, send input to server
+
+// 1. connect to server
+
+// 2. receive continous updates to gamestate
+
+// 3. continuously render the gamestate
+
+// 4. send input to server
